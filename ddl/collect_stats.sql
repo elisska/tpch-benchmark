8c6cb9344ac894715211d312d6ca0c6b49696354
Oleksandra Klevets <oklev@softserveinc.com>
2017-10-17 12:30:05 +0300

    Create collect_stats.sql

---
+++ ddl/collect_stats.sql
@@ -0,0 +1,8 @@
+COMPUTE STATS lineitem_parquet;
+COMPUTE STATS orders_parquet;
+COMPUTE STATS supplier_parquet;
+COMPUTE STATS nation_parquet;
+COMPUTE STATS part_parquet;
+COMPUTE STATS partsupp_parquet;
+COMPUTE STATS region_parquet;
+COMPUTE STATS customer_parquet;
